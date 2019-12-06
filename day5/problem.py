e2c92934fb2ead242f39a2fe9e7a776858846fac
Stephen Devine <ste@stedevine.com>
2019-12-05 21:29:10 -0800

    day5 part 2

--- day5/problem.py
+++ day5/problem.py
@@ -7,7 +7,7 @@ def process_tape(tape, input):
         # The rightmost 2 digits are the opcode
         # We can ignore the leading 0
         op_code = instruction % 100
-        #print('position {} opcode {}'.format(position, op_code))
+        print('position {} opcode {}'.format(position, op_code))
         param_mode_1 = int((instruction % 1000) / 100)
         param_mode_2 = int((instruction % 10000) / 1000)
         param_mode_3 = int((instruction % 100000) / 10000)
@@ -16,28 +16,75 @@ def process_tape(tape, input):
         if op_code == 1 or op_code == 2:
             param_1 = tape[tape[position + 1]] if param_mode_1 == 0 else tape[position + 1]
             param_2 = tape[tape[position + 2]] if param_mode_2 == 0 else tape[position + 2]
-            output_index = tape[position + 3] if param_mode_3 == 0 else position + 3
+            param_3 = tape[position + 3] if param_mode_3 == 0 else position + 3
             if op_code == 1:
-                tape[output_index] = param_1 + param_2
+                tape[param_3] = param_1 + param_2
             elif op_code == 2:
-                tape[output_index] = param_1 * param_2
+                tape[param_3] = param_1 * param_2
             position = position + 4
 
         elif op_code == 3 or op_code == 4:
-            output_index = tape[position + 1] if param_mode_1 == 0 else position + 1
+            param_1 = tape[position + 1] if param_mode_1 == 0 else position + 1
             if op_code == 3:
-                tape[output_index] = input
+                tape[param_1] = input
             elif op_code == 4:
-                output = tape[output_index]
+                output = tape[param_1]
                 #print('output is {}'.format(output))
             position = position + 2
 
+        elif op_code == 5:
+            param_1 = tape[tape[position + 1]] if param_mode_1 == 0 else tape[position + 1]
+            param_2 = tape[tape[position + 2]] if param_mode_2 == 0 else tape[position + 2]
+            if param_1 != 0:
+                position = param_2
+            else:
+                position = position + 3
+
+        elif op_code == 6:
+            param_1 = tape[tape[position + 1]] if param_mode_1 == 0 else tape[position + 1]
+            param_2 = tape[tape[position + 2]] if param_mode_2 == 0 else tape[position + 2]
+            if param_1 == 0:
+                position = param_2
+            else:
+                position = position + 3
+
+        elif op_code == 7:
+            param_1 = tape[tape[position + 1]] if param_mode_1 == 0 else tape[position + 1]
+            param_2 = tape[tape[position + 2]] if param_mode_2 == 0 else tape[position + 2]
+            param_3 = tape[position + 3] if param_mode_3 == 0 else position + 3
+            tape[param_3] = 1 if (param_1 < param_2) else 0
+            position = position + 4
+
+        elif op_code == 8:
+            print(tape)
+
+            param_1 = tape[tape[position + 1]] if param_mode_1 == 0 else tape[position + 1]
+            param_2 = tape[tape[position + 2]] if param_mode_2 == 0 else tape[position + 2]
+            param_3 = tape[position + 3] if param_mode_3 == 0 else position + 3
+            print('param_mode_1 {} param_1 {}'.format(param_mode_1, param_1))
+            print('param_mode_2 {} param_2 {}'.format(param_mode_2, param_2))
+            print('param_mode_3 {} param_3 {}'.format(param_mode_3, param_3))
+
+            tape[param_3] = 1 if (param_1 == param_2) else 0
+            print('{}'.format(tape))
+            position = position + 4
+
+
         elif op_code == 99:
             return output
+
+        else:
+            return 'error {}'.format(op_code)
 
     return None
 
 tape = [3,225,1,225,6,6,1100,1,238,225,104,0,1102,7,85,225,1102,67,12,225,102,36,65,224,1001,224,-3096,224,4,224,1002,223,8,223,101,4,224,224,1,224,223,223,1001,17,31,224,1001,224,-98,224,4,224,1002,223,8,223,101,5,224,224,1,223,224,223,1101,86,19,225,1101,5,27,225,1102,18,37,225,2,125,74,224,1001,224,-1406,224,4,224,102,8,223,223,101,2,224,224,1,224,223,223,1102,13,47,225,1,99,14,224,1001,224,-98,224,4,224,102,8,223,223,1001,224,2,224,1,224,223,223,1101,38,88,225,1102,91,36,224,101,-3276,224,224,4,224,1002,223,8,223,101,3,224,224,1,224,223,223,1101,59,76,224,1001,224,-135,224,4,224,102,8,223,223,1001,224,6,224,1,223,224,223,101,90,195,224,1001,224,-112,224,4,224,102,8,223,223,1001,224,7,224,1,224,223,223,1102,22,28,225,1002,69,47,224,1001,224,-235,224,4,224,1002,223,8,223,101,5,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,107,226,226,224,102,2,223,223,1006,224,329,1001,223,1,223,1107,677,226,224,1002,223,2,223,1005,224,344,101,1,223,223,108,677,226,224,102,2,223,223,1006,224,359,101,1,223,223,7,677,226,224,102,2,223,223,1005,224,374,101,1,223,223,1008,677,226,224,1002,223,2,223,1006,224,389,1001,223,1,223,7,226,677,224,102,2,223,223,1005,224,404,101,1,223,223,1007,226,226,224,102,2,223,223,1006,224,419,101,1,223,223,7,226,226,224,102,2,223,223,1005,224,434,1001,223,1,223,8,226,226,224,1002,223,2,223,1006,224,449,101,1,223,223,1007,677,677,224,102,2,223,223,1006,224,464,101,1,223,223,1007,226,677,224,1002,223,2,223,1006,224,479,101,1,223,223,108,226,226,224,102,2,223,223,1005,224,494,1001,223,1,223,1108,677,677,224,102,2,223,223,1005,224,509,1001,223,1,223,107,226,677,224,1002,223,2,223,1005,224,524,101,1,223,223,1108,677,226,224,1002,223,2,223,1005,224,539,1001,223,1,223,1008,677,677,224,1002,223,2,223,1006,224,554,101,1,223,223,1008,226,226,224,102,2,223,223,1005,224,569,1001,223,1,223,8,677,226,224,102,2,223,223,1006,224,584,101,1,223,223,107,677,677,224,102,2,223,223,1006,224,599,101,1,223,223,8,226,677,224,102,2,223,223,1006,224,614,101,1,223,223,1107,226,677,224,102,2,223,223,1006,224,629,101,1,223,223,108,677,677,224,1002,223,2,223,1005,224,644,1001,223,1,223,1107,226,226,224,102,2,223,223,1005,224,659,101,1,223,223,1108,226,677,224,102,2,223,223,1005,224,674,101,1,223,223,4,223,99,226]
 # Problem 1 (input = 1, support only 4 opcodes)
-print(process_tape(tape, 1))
-#print(tape)
+print(process_tape(tape.copy(), 1))
+# Problem 2, input = 5, support more opcodes
+print(process_tape(tape.copy(), 5))
+test_1 = [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]
+test_2 = [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]
+test_3 = [3,9,8,9,10,9,4,9,99,-1,8]
+test_4 = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+print(process_tape(test_4, 9))
