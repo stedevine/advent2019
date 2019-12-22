283fcb4f963d08938c2d3a527bff042921748b80
Stephen Devine <ste@stedevine.com>
2019-12-21 18:26:40 -0800

    part 2

--- day10/problem.py
+++ day10/problem.py
@@ -44,7 +44,6 @@ def get_board(filename):
 # For every point on the board for which there is another asteroid
 # Get the line of site
 # If line of site is all . - can see asteroid, otherwise blocked
-
 def get_max_number_visible_asteroids(asteroids):
     max_count = 0
     for i in range(0, len(asteroids)):
@@ -59,16 +58,40 @@ def get_max_number_visible_asteroids(asteroids):
         max_count = max(count,max_count)
     return max_count
 
+'''
+print(get_max_number_visible_asteroids(get_asteroids(get_board('./test1.txt'))))
+print(get_max_number_visible_asteroids(get_asteroids(get_board('./test2.txt'))))
+print(get_max_number_visible_asteroids(get_asteroids(get_board('./test3.txt'))))
+print(get_max_number_visible_asteroids(get_asteroids(get_board('./test4.txt'))))
+print(get_max_number_visible_asteroids(get_asteroids(get_board('./input.txt'))))
+'''
+
+def angle(origin, target):
+    # Angle for right triangle is tan^-1(opposite/adjacent)
+    # The opposite and adjecent values depend on what quadrant the target is
+    # in relative to the origin
+    delta_x = target[0] - origin[0]
+    print(delta_x)
+    # increasing y moves a point DOWNWARDS
+    delta_y = -(target[1] - origin[1])
+    print(delta_y)
+    if (delta_x >= 0 and delta_y >= 0):
+        # top right
+        return 0 if delta_y == 0 else math.degrees(math.atan(delta_x/delta_y))
+    if (delta_x >= 0 and delta_y <= 0):
+        # bottom right
+        return 90 if delta_x == 0 else 90 + (-1) * math.degrees(math.atan(delta_y/delta_x))
+    if (delta_x <= 0 and delta_y <= 0):
+        # bottom left
+        return 180 if delta_y == 0 else 180 + (-1) * math.degrees(math.atan(delta_x/delta_y))
 
 
+    return None
 
-board = get_board('./test1.txt')
-print(get_max_number_visible_asteroids(get_asteroids(board)))
-board = get_board('./test2.txt')
-print(get_max_number_visible_asteroids(get_asteroids(board)))
-board = get_board('./test3.txt')
-print(get_max_number_visible_asteroids(get_asteroids(board)))
-board = get_board('./test4.txt')
-print(get_max_number_visible_asteroids(get_asteroids(board)))
-board = get_board('./input.txt')
-print(get_max_number_visible_asteroids(get_asteroids(board)))
+## Problem 2
+asteroids = get_asteroids(get_board('./test5.txt'))
+print(asteroids)
+# Starting at (8,3) get the angle between the vertical line and the line that connects the origin to the target
+print(angle((2,3), (3,0)))
+print(angle((2,3), (4,6)))
+print(angle((2,3), (4,0`)))
