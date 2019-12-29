8b42bdd4589a7f932e92fa4071a540466fcb1544
Stephen Devine <ste@stedevine.com>
2019-12-29 12:10:16 -0800

    refactor

--- day12/problem.py
+++ day12/problem.py
@@ -1,5 +1,7 @@
 from itertools import combinations
 import math
+import copy
+
 class Moon:
     def __init__(self, x, y, z):
         self.x = x
@@ -44,18 +46,6 @@ def get_energy(moons):
 
     return total
 
-def problem1():
-    moons = [ Moon(-13,-13,-13), Moon(5,-8,3), Moon(-6,-10,-3), Moon(0,5,-5)]
-    #moons = [ Moon(-1,0,2), Moon(2,-10,-7), Moon(4,-8,8), Moon(3,5,-1)]
-    for i in range(0,1000):
-        for pair in combinations(moons,2):
-            update_moon_velocities(pair[0],pair[1])
-        do_step(moons)
-
-    print(get_energy(moons))
-
-problem1()
-
 
 def check_moonal_x(moons,original_moons):
     for i in range(0, len(moons)):
@@ -78,40 +68,21 @@ def check_moonal_z(moons,original_moons):
             return False
     return True
 
-def get_lowest_common_multiple(x,y,z):
-    xs = set()
-    ys = set()
-    zs = set()
-    multiplier = 0
-    common = set()
-    while (len(common) == 0):
-        multiplier = multiplier + 1
-        if (multiplier % 1000 == 0):
-            print(multiplier)
-        xs.add(multiplier * x)
-        ys.add(multiplier * y)
-        zs.add(multiplier * z)
-        #print('{} {}'.format(multiplier, xs))
-        common = xs.intersection(ys).intersection(zs)
-
-    return common.pop()
-
 def get_lcm(x,y):
     return int(x * y / math.gcd(x,y))
 
-def problem2():
+def problem1(moons):
+    for i in range(0,1000):
+        for pair in combinations(moons,2):
+            update_moon_velocities(pair[0],pair[1])
+        do_step(moons)
 
-    # Test 1
-    #moons =          [ Moon(-1,0,2), Moon(2,-10,-7), Moon(4,-8,8), Moon(3,5,-1)]
-    #original_moons = [ Moon(-1,0,2), Moon(2,-10,-7), Moon(4,-8,8), Moon(3,5,-1)]
+    print(get_energy(moons))
 
-    # Test 2
-    #moons =          [ Moon(-8,-10,0), Moon(5,5,10), Moon(2,-7,3), Moon(9,-8,-3)]
-    #original_moons = [ Moon(-8,-10,0), Moon(5,5,10), Moon(2,-7,3), Moon(9,-8,-3)]
 
-    # Puzzle input
-    moons = [ Moon(-13,-13,-13), Moon(5,-8,3), Moon(-6,-10,-3), Moon(0,5,-5)]
-    original_moons = [ Moon(-13,-13,-13), Moon(5,-8,3), Moon(-6,-10,-3), Moon(0,5,-5)]
+
+def problem2(moons):
+    original_moons = copy.deepcopy(moons)
 
     # Get each orbital plane period for all moons
     x_period = 0
@@ -137,12 +108,21 @@ def problem2():
             z_period = steps
             print('moonal Z match at {}'.format(z_period))
 
-    print('x {}'.format(x_period))
-    print('y {}'.format(y_period))
-    print('z {}'.format(z_period))
+    print('x period {}'.format(x_period))
+    print('y period {}'.format(y_period))
+    print('z period {}'.format(z_period))
 
     # The lowest common multiple of these periods is the number of steps required
     # for the entire system to return to the initial state.
     print(get_lcm(get_lcm(x_period,y_period),z_period))
 
-problem2()
+# Test 1
+#moons =          [ Moon(-1,0,2), Moon(2,-10,-7), Moon(4,-8,8), Moon(3,5,-1)]
+
+# Test 2
+#moons =          [ Moon(-8,-10,0), Moon(5,5,10), Moon(2,-7,3), Moon(9,-8,-3)]
+
+# Puzzle input
+moons = [ Moon(-13,-13,-13), Moon(5,-8,3), Moon(-6,-10,-3), Moon(0,5,-5)]
+problem1(moons)
+problem2(moons)
